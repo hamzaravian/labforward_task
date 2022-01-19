c76a3db5dbd68f2f0680cba723258fa505450b11
Hamza Mushtaq <gcu.hamza@gmail.com>
2022-01-19 22:13:58 +0500

    added params

--- app/controllers/api/v1/identify_peaks_controller.rb
+++ app/controllers/api/v1/identify_peaks_controller.rb
@@ -1,6 +1,13 @@
 class Api::V1::IdentifyPeaksController < ApplicationController
 
     def peaks_calculator
-        
+
     end
+
+    private
+
+    def identify_peaks_params
+      params.permit( {:sample_data => []}, :threshold )
+    end
+    
 end
